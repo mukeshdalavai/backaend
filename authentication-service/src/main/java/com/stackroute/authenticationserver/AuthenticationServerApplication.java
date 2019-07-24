33b76b045d57521e8f6d127fba3d59b360711bbf
Mukesh <mukeshdalavai@gmail.com>
2019-07-24 12:00:33 +0530

    Initial Commit

---
+++ authentication-service/src/main/java/com/stackroute/authenticationserver/AuthenticationServerApplication.java
@@ -0,0 +1,15 @@
+package com.stackroute.authenticationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class AuthenticationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServerApplication.class, args);
+	}
+
+}
